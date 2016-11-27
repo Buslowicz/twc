4d4421d1f72140691d18ca12939d1c304603401e
Daniel Busłowicz <draccoz@gmail.com>
2016-11-28 00:54:02 +0100

    Git config refactors

---
+++ tests/assets/element-name.d.ts
@@ -0,0 +1,2 @@
+import "link!bower_components/polymer/polymer.html";
+import "link!node_modules/easy-polymer/dist/esp.html";
---
+++ tests/assets/element-name.js
@@ -0,0 +1,7 @@
+import "link!bower_components/polymer/polymer.html";
+import "link!node_modules/easy-polymer/dist/esp.html";
+class ElementName {
+    constructor() {
+        this.test = "tester";
+    }
+}
