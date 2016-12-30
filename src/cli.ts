988808f28e3929a42ffc0025aa04bf0f6fe8782b
Daniel Busłowicz <draccoz@gmail.com>
2016-12-30 19:40:42 +0100

    twc-32 Allow passing json config files location in CLI and function options

--- src/cli.ts
+++ src/cli.ts
@@ -1,19 +1,25 @@
 #!/usr/bin/env node
 
-import program = require("commander");
+import { join } from "path";
 import { src, dest } from "gulp";
-import ts2html = require("./index");
+import program = require("commander");
+import twc = require("./index");
 
 if (require.main === module) {
   program
-    .option("-o, --out-dir <value>", "Output file")
+    .option("-t, --ts-config <path>", "tsconfig.json file location")
+    .option("-b, --bower-config <path>", "bower.json file location")
+    .option("-o, --out-dir <path>", "Output file")
     .parse(process.argv);
 
   let cwd = process.cwd();
   let files = program.args;
 
   console.time("Done");
-  ts2html(src(files.length === 0 ? [ "*.ts" ] : files, { cwd }))
+  twc(src(files.length === 0 ? [ "*.ts" ] : files, { cwd }), {
+    tsConfigPath: program[ "tsConfig" ] ? join(process.cwd(), program[ "tsConfig" ]) : undefined,
+    bowerConfigPath: program[ "bowerConfig" ] ? join(process.cwd(), program[ "bowerConfig" ]) : undefined
+  })
     .pipe(dest(program[ "outDir" ] || "out", { cwd }))
     .on("finish", () => {
       console.timeEnd("Done");
